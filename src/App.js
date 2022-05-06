b1d9d9cdd6f3792f092bb0b1f5ed364cb10dab74
linhnguyen <linh.nguyenthikhanh23@student.passerellesnumeriques.org>
2022-05-06 14:35:05 +0700

    upload

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state ={
-            items:[],
+            products:[],
             isLoaded: false,
         }
     }
@@ -16,45 +16,29 @@ class App extends Component {
             .then(json => {
                 this.setState({
                     isLoaded: true,
-                    items: json,
+                    products: json,
                 })
             })
     }
 
-    // callAPI(){
-    //     this.setState({
-    //         isLoaded: true,
-    //     });
-    //     fetch("https://625bcc9a398f3bc782aed2bb.mockapi.io/products")
-    //         .then(res => res.json())
-    //         .then(
-    //             (result) => {
-    //                 this.setState({
-    //                     isLoaded: false,
-    //                     items: result.items
-    //                 });
-    //             }
-    //         )
-    // }
-
     render() {
-        const {isLoaded, items} = this.state;
+        const {isLoaded, products} = this.state;
 
         if(!isLoaded){
             return <div>Loading...</div>;
         }
         return (
             <div>
-                <h2>MACBOOK SIU XỊN</h2>
+                <h2>MACBOOK</h2>
                 <div className="wrapper">
                     {/* eslint-disable-next-line array-callback-return */}
-                    {items.map(item => {
+                    {products.map(product => {
                         return(
-                            <div className="macbook__cart" key={item.id}>
-                                <img src={item.image} alt=""/>
-                                <p>{item.title}</p>
-                                <p>{item.price}</p>
-                                <p>{item.offer}</p>
+                            <div className="macbook__cart" key={product.id}>
+                                <img src={product.image} alt=""/>
+                                <p>{product.title}</p>
+                                <p>{product.price}</p>
+                                <p>{product.offer}</p>
                             </div>
                         )
                     })}
